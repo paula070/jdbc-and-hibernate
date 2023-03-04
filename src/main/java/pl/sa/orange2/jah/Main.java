c5a523b1d11bfab1b9f4ce9f9b6c3bd1026a7bb3
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-03-04 11:27:14 +0100

    [change] - more output to console

--- src/main/java/pl/sa/orange2/jah/Main.java
+++ src/main/java/pl/sa/orange2/jah/Main.java
@@ -3,5 +3,6 @@ package pl.sa.orange2.jah;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Works...");
+        System.out.println("JDBC and Hibernate - love: ");
     }
 }
