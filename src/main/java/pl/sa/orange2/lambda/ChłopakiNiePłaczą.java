ab0248f2267961bc20be5a6a70bcb49045c41916
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-03-04 13:34:05 +0100

    [change] - interface with implementation - polimorphism

---
+++ src/main/java/pl/sa/orange2/lambda/ChłopakiNiePłaczą.java
@@ -0,0 +1,12 @@
+package pl.sa.orange2.lambda;
+
+public class ChłopakiNiePłaczą implements BoysDontCry{
+    @Override
+    public void silnoreki() {
+        System.out.println("Jestem silnoreki:)");
+    }
+
+    public void sing() {
+        System.out.println("Ruszaj się...");
+    }
+}
--- src/main/java/pl/sa/orange2/lambda/LambdaExercises.java
+++ src/main/java/pl/sa/orange2/lambda/LambdaExercises.java
@@ -3,5 +3,10 @@ package pl.sa.orange2.lambda;
 public class LambdaExercises {
 
     public static void main(String[] args) {
+
+        BoysDontCry boy = new ChłopakiNiePłaczą();
+        boy.silnoreki();
+        ChłopakiNiePłaczą boy2 = new ChłopakiNiePłaczą();
+        boy2.sing();
     }
 }
