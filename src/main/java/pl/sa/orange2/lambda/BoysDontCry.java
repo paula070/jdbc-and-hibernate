3d1d69f7b4d32edd89d6b658488739be262b42b5
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-03-05 08:52:15 +0100

    [change] - using @FunctionalInterface and describing its purpose
    1) @FunctionalInterface annotation
    2) Default methods in interfaces

--- src/main/java/pl/sa/orange2/lambda/BoysDontCry.java
+++ src/main/java/pl/sa/orange2/lambda/BoysDontCry.java
@@ -1,7 +1,10 @@
 package pl.sa.orange2.lambda;
 // to jest interfejs funkcyjny poniewaz ma jedna abstrakcyjna metode
+@FunctionalInterface
+
 public interface BoysDontCry {
 
     void silnoreki();
 
+    default void cry () {};
 }
