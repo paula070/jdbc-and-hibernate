c4a6677be5b59476923c95bca173b720188e554a
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-03-05 14:50:19 +0100

    [add] - h2 connection parameters

---
+++ src/main/java/pl/sa/orange2/jdbc/config/H2Config.java
@@ -0,0 +1,11 @@
+package pl.sa.orange2.jdbc.config;
+
+public class H2Config {
+
+    public final static String DB_URL = "jdbc:h2:~/databases/jdbc";
+
+    public final static String USER = "sa";
+
+    public final static String PASSWORD = "sa";
+
+}
