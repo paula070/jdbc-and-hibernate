c6ec94c5fc7853c041f32014761858d7a558cecd
paulinakoprowska <paulina.koprowska@opoczta.pl>
2023-03-07 21:03:27 +0100

    [add] - Car dao created

---
+++ src/main/java/pl/sa/orange2/dao/CarDao.java
@@ -0,0 +1,24 @@
+package pl.sda.orange2.dao;
+
+import pl.sda.orange2.entity.Car;
+
+import java.util.List;
+
+public class CarDao {
+
+    public void save(Car car) {
+
+    }
+
+    public List<Car> findAll() {
+        return List.of();
+    }
+
+    public Car findById(Long id) {
+        return null;
+    }
+
+    public void deleteById(Long id) {
+
+    }
+}
